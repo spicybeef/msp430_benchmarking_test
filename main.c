9773ee1abe0fef51d397ba165dc9352568b3068c
Michel Kakulphimp <michel@kakulphimp.net>
2020-02-22 21:26:30 -0800

    Bringing over example code

--- main.c
+++ main.c
@@ -1,41 +1,190 @@
-/* --COPYRIGHT--,BSD
- * Copyright (c) 2017, Texas Instruments Incorporated
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * *  Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * *  Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * *  Neither the name of Texas Instruments Incorporated nor the names of
- *    its contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * --/COPYRIGHT--*/
+
 #include "driverlib.h"
 
-//******************************************************************************
-//!
-//!   Empty Project that includes driverlib
-//!
-//******************************************************************************
+//-----------------------------------------------------------------------------
+
+Calendar calendar;                                // Calendar used for RTC
+
+//-----------------------------------------------------------------------------
+
+void Init_GPIO(void);
+void Init_Clock(void);
+void Init_UART(void);
+void Init_RTC(void);
+void enterLPM35();
+
+//-----------------------------------------------------------------------------
+int _system_pre_init(void)
+{
+    // Stop Watchdog timer
+    WDT_A_hold(__MSP430_BASEADDRESS_WDT_A__);     // Stop WDT
+
+    /*==================================*/
+    /* Choose if segment initialization */
+    /* should be done or not. */
+    /* Return: 0 to omit initialization */
+    /* 1 to run initialization */
+    /*==================================*/
+    return 1;
+}
+
 void main (void)
 {
+    Init_GPIO();
+    Init_Clock();
+    Init_UART();
+
+    __bis_SR_register(LPM3_bits | GIE);       // Enter LPM3 and wait for PC commands
+    __no_operation();
+}
+
+/*
+ * GPIO Initialization
+ */
+void Init_GPIO()
+{
+    // Set all GPIO pins to output low for low power
+    GPIO_setOutputLowOnPin(GPIO_PORT_P1, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setOutputLowOnPin(GPIO_PORT_P2, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setOutputLowOnPin(GPIO_PORT_P3, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setOutputLowOnPin(GPIO_PORT_P4, GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setOutputLowOnPin(GPIO_PORT_P5, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setOutputLowOnPin(GPIO_PORT_P6, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setOutputLowOnPin(GPIO_PORT_P7, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setOutputLowOnPin(GPIO_PORT_P8, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setOutputLowOnPin(GPIO_PORT_PJ, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7|GPIO_PIN8|GPIO_PIN9|GPIO_PIN10|GPIO_PIN11|GPIO_PIN12|GPIO_PIN13|GPIO_PIN14|GPIO_PIN15);
+    GPIO_setOutputHighOnPin(GPIO_PORT_P4, GPIO_PIN0);
+
+    GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setAsOutputPin(GPIO_PORT_P2, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setAsOutputPin(GPIO_PORT_P3, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setAsOutputPin(GPIO_PORT_P4, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setAsOutputPin(GPIO_PORT_P5, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setAsOutputPin(GPIO_PORT_P6, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setAsOutputPin(GPIO_PORT_P7, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setAsOutputPin(GPIO_PORT_P8, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7);
+    GPIO_setAsOutputPin(GPIO_PORT_PJ, GPIO_PIN0|GPIO_PIN1|GPIO_PIN2|GPIO_PIN3|GPIO_PIN4|GPIO_PIN5|GPIO_PIN6|GPIO_PIN7|GPIO_PIN8|GPIO_PIN9|GPIO_PIN10|GPIO_PIN11|GPIO_PIN12|GPIO_PIN13|GPIO_PIN14|GPIO_PIN15);
+
+    // Configure P2.0 - UCA0TXD and P2.1 - UCA0RXD
+    GPIO_setOutputLowOnPin(GPIO_PORT_P2, GPIO_PIN0);
+    GPIO_setAsOutputPin(GPIO_PORT_P2, GPIO_PIN0);
+    GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P2, GPIO_PIN1, GPIO_SECONDARY_MODULE_FUNCTION);
+
+    // Set PJ.4 and PJ.5 as Primary Module Function Input, LFXT.
+    GPIO_setAsPeripheralModuleFunctionInputPin(
+           GPIO_PORT_PJ,
+           GPIO_PIN4 + GPIO_PIN5,
+           GPIO_PRIMARY_MODULE_FUNCTION
+           );
+
+    // Disable the GPIO power-on default high-impedance mode
+    // to activate previously configured port settings
+    PMM_unlockLPM5();
+}
+
+/*
+ * Clock System Initialization
+ */
+void Init_Clock()
+{
+    // Set DCO frequency to 8 MHz
+    CS_setDCOFreq(CS_DCORSEL_0, CS_DCOFSEL_6);
+    //Set external clock frequency to 32.768 KHz
+    CS_setExternalClockSource(32768, 0);
+    //Set ACLK=LFXT
+    CS_initClockSignal(CS_ACLK, CS_LFXTCLK_SELECT, CS_CLOCK_DIVIDER_1);
+    // Set SMCLK = DCO with frequency divider of 1
+    CS_initClockSignal(CS_SMCLK, CS_DCOCLK_SELECT, CS_CLOCK_DIVIDER_1);
+    // Set MCLK = DCO with frequency divider of 1
+    CS_initClockSignal(CS_MCLK, CS_DCOCLK_SELECT, CS_CLOCK_DIVIDER_1);
+    //Start XT1 with no time out
+    CS_turnOnLFXT(CS_LFXT_DRIVE_3);
+}
+
+/*
+ * UART Communication Initialization
+ */
+void Init_UART()
+{
+    // Configure UART
+    EUSCI_A_UART_initParam param = {0};
+    param.selectClockSource = EUSCI_A_UART_CLOCKSOURCE_SMCLK;
+    param.clockPrescalar = 52;
+    param.firstModReg = 1;
+    param.secondModReg = 0x49;
+    param.parity = EUSCI_A_UART_NO_PARITY;
+    param.msborLsbFirst = EUSCI_A_UART_LSB_FIRST;
+    param.numberofStopBits = EUSCI_A_UART_ONE_STOP_BIT;
+    param.uartMode = EUSCI_A_UART_MODE;
+    param.overSampling = EUSCI_A_UART_OVERSAMPLING_BAUDRATE_GENERATION;
+
+    if(STATUS_FAIL == EUSCI_A_UART_init(EUSCI_A0_BASE, &param))
+        return;
+
+    EUSCI_A_UART_enable(EUSCI_A0_BASE);
+
+    EUSCI_A_UART_clearInterrupt(EUSCI_A0_BASE,
+                                EUSCI_A_UART_RECEIVE_INTERRUPT);
+
+    // Enable USCI_A0 RX interrupt
+    EUSCI_A_UART_enableInterrupt(EUSCI_A0_BASE,
+                                 EUSCI_A_UART_RECEIVE_INTERRUPT); // Enable interrupt
+
+    // Enable globale interrupt
+    __enable_interrupt();
+}
+
+/*
+ * Real Time Clock Initialization
+ */
+void Init_RTC()
+{
+    //Setup Current Time for Calendar
+    calendar.Seconds    = 0x55;
+    calendar.Minutes    = 0x30;
+    calendar.Hours      = 0x04;
+    calendar.DayOfWeek  = 0x01;
+    calendar.DayOfMonth = 0x30;
+    calendar.Month      = 0x04;
+    calendar.Year       = 0x2014;
+
+    // Initialize RTC with the specified Calendar above
+    RTC_C_initCalendar(RTC_C_BASE,
+                       &calendar,
+                       RTC_C_FORMAT_BCD);
+
+    RTC_C_setCalendarEvent(RTC_C_BASE,
+                           RTC_C_CALENDAREVENT_MINUTECHANGE
+                           );
+
+    RTC_C_clearInterrupt(RTC_C_BASE,
+                         RTC_C_TIME_EVENT_INTERRUPT
+                         );
+
+    RTC_C_enableInterrupt(RTC_C_BASE,
+                          RTC_C_TIME_EVENT_INTERRUPT
+                          );
+
+    //Start RTC Clock
+    RTC_C_startClock(RTC_C_BASE);
+}
+
+/*
+ * Enter Low Power Mode 3.5
+ */
+void enterLPM35()
+{
+    // Configure button S2 (P5.5) interrupt
+    GPIO_selectInterruptEdge(GPIO_PORT_P5, GPIO_PIN5, GPIO_HIGH_TO_LOW_TRANSITION);
+    GPIO_setAsInputPinWithPullUpResistor(GPIO_PORT_P5, GPIO_PIN5);
+    GPIO_clearInterrupt(GPIO_PORT_P5, GPIO_PIN5);
+    GPIO_enableInterrupt(GPIO_PORT_P5, GPIO_PIN5);
+
+    // Request the disabling of the core voltage regulator when device enters
+    // LPM3 (or LPM4) so that we can effectively enter LPM3.5 (or LPM4.5).
+    PMM_turnOffRegulator();
+
+    //Enter LPM3 mode with interrupts enabled
+    __bis_SR_register(LPM4_bits + GIE);
+    __no_operation();
 }
